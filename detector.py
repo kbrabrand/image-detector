ff9239929e046ea2b5f9af2da898218c53ac6618
Kristoffer Brabrand <kristoffer@brabrand.no>
2015-03-03 19:48:14 +0100

    Take one image as param. Changed threshold value from 150 to 180 (0-255)

--- detector.py
+++ detector.py
@@ -1,34 +1,30 @@
-import os
 from sys import argv
+from scipy import ndimage
 import numpy as np
 import scipy
-from scipy import ndimage
 
 def returnObjects (file):
-    print ''
-    print 'FILE::', file
-
     im = scipy.misc.imread(file, flatten=1)
-    im = np.where(im > 150, 0, 1)
+    im = np.where(im > 180, 0, 1)
     label_im, num = ndimage.label(im)
     slices = ndimage.find_objects(label_im)
-    centroids = ndimage.measurements.center_of_mass(im, label_im, xrange(1,num+1))
 
-    angles = []
+    print '['
+
+    counter = 0;
     for s in slices:
         height, width = label_im[s].shape
 
         if width > 35:
-            print '  OBJECTSTART::'
-            print '    upper left:', s[1].start, ',', s[0].start
-            print '    lower right:', s[1].start + width, ',', s[0].start+height
-            print '  OBJECTEND::'
-            print ''
+            prefix = ',' if counter > 0 else ''
+
+            print '%s{"x":%d,"y":%d,"width":%d,"height":%d}' % (prefix, s[1].start, s[0].start, width, height)
+
+            # Increase counter
+            counter += 1
+
+    print ']'
 
 # If-test to ensure code only executed if ran as stand-alone app.
 if __name__ == "__main__":
-     rootdir = argv[1];
-
-     for subdir, dirs, files in os.walk(rootdir):
-        for file in files:
-            returnObjects(os.path.join(subdir, file))
+     returnObjects(argv[1])
